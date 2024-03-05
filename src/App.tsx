f2b80fef0de69bb7c7a507f5237168820d8cf5d9
erfan taheri <erfanta197@gmail.com>
2024-03-06 01:11:46 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import CardBox from "./components/CardBox";
+
+function App() {
+  return (
+    <Box height="100vh">
+      <CardBox />
+    </Box>
+  );
+}
+
+export default App;
