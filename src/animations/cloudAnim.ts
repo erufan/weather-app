bfc4aaa0450bde278c575aa56741a14c25f75988
erfan taheri <erfanta197@gmail.com>
2024-04-07 00:41:28 +0330

    fix bug clouds freez when click at same city name twice

--- src/animations/cloudAnim.ts
+++ src/animations/cloudAnim.ts
@@ -2,23 +2,27 @@ import gsap from "gsap";
 import Current from "../interfaces/Current";
 
 const cloudAnim = (weather: Current | undefined) => {
-  const noPrecipitation = [1, 2, 3, 45, 48];
+  const precipitationExist = [
+    51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 71, 73, 75, 77, 80, 81, 82, 85, 86,
+  ];
 
   const dublicatedCode = gsap
     .timeline({
       defaults: { duration: 6, repeat: -1, ease: "none" },
     })
-    .to(".cloud1", { x: "-150%" })
-    .to(".cloud2", { x: "-150%" }, 2.5)
-    .to(".cloud3", { x: "-200%" }, 3);
+    .fromTo(".cloud1", { x: "210%" }, { x: "-150%" })
+    .fromTo(".cloud2", { x: "210%" }, { x: "-150%" }, 2.5)
+    .fromTo(".cloud3", { x: "210%" }, { x: "-200%" }, 3);
 
-  noPrecipitation.find((e) => {
-    if (e === weather?.weather_code) return dublicatedCode;
-    dublicatedCode.to(
-      ".precipitation",
-      { y: "150%", x: "-150%", duration: 3 },
-      0
-    );
+  precipitationExist.find((e) => {
+    e === weather?.weather_code &&
+      dublicatedCode.fromTo(
+        ".precipitation",
+        { y: "0", x: "0", duration: 3 },
+        { y: "150%", x: "-150%", duration: 3 },
+        0
+      );
+    dublicatedCode;
   });
 };
 
