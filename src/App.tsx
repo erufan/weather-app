06b49df0fe79e3234ea08e0125ce1ef982158a16
erfan taheri <erfanta197@gmail.com>
2024-04-10 03:07:39 +0330

    refactoring move Context to one uper parent(for reusable card)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { AbsoluteCenter, Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import CardBox from "./components/CardBox";
+import InputContext from "./context/InputContext";
+import LocationContext from "./context/LocationContext";
+import Location from "./interfaces/Location";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState<string | undefined>("");
+  const [location, setLocation] = useState<Location>({
+    city: "Tehran",
+    latitude: 35.6875,
+    longitude: 51.4375,
+    country: "Iran",
+    country_code: "IR",
+  });
+
   return (
-    <Box height="100vh" backgroundColor="teal.700">
-      <AbsoluteCenter width="400px">
-        <CardBox />
-      </AbsoluteCenter>
-    </Box>
+    <InputContext.Provider value={{ input, setInput }}>
+      <LocationContext.Provider value={{ location, setLocation }}>
+        <Box backgroundColor="teal.700" height="100vh">
+          <HStack justifyContent="center">
+            <CardBox type="main" />
+          </HStack>
+        </Box>
+      </LocationContext.Provider>
+    </InputContext.Provider>
   );
 }
 
--- src/components/CardBox.tsx
+++ src/components/CardBox.tsx
@@ -1,37 +1,20 @@
-import { Card, CardHeader, CardBody, HStack } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody } from "@chakra-ui/react";
 import MeteorologyInfo from "./MeteorologyInfo";
-import { useState } from "react";
-import InputContext from "../context/InputContext";
-import LocationContext from "../context/LocationContext";
-import Location from "../interfaces/Location";
 import Top from "./aboutCity/Top";
 
 const CardBox = ({ type }: { type: "main" | "minor" }) => {
-  const [input, setInput] = useState<string | undefined>("");
-  const [location, setLocation] = useState<Location>({
-    city: "Tehran",
-    latitude: 35.6875,
-    longitude: 51.4375,
-    country: "Iran",
-    country_code: "IR",
-  });
-
   return (
     <>
-      <InputContext.Provider value={{ input, setInput }}>
-        <LocationContext.Provider value={{ location, setLocation }}>
-          <Card backgroundColor="teal.400" padding=" 14.4px" width="400px">
-            {type === "main" && (
-              <CardHeader>
-                <Top />
-              </CardHeader>
-            )}
-            <CardBody>
-              <MeteorologyInfo />
-            </CardBody>
-          </Card>
-        </LocationContext.Provider>
-      </InputContext.Provider>
+      <Card backgroundColor="teal.400" padding=" 14.4px" width="400px">
+        {type === "main" && (
+          <CardHeader>
+            <Top />
+          </CardHeader>
+        )}
+        <CardBody>
+          <MeteorologyInfo />
+        </CardBody>
+      </Card>
     </>
   );
 };
